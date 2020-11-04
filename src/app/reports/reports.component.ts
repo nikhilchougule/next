6522ab07ca5df4d55fc7dcd6479b2c7f3e8e1de0
mohinironghe <mohini.ronghe@techinvento.com>
2020-11-04 12:07:39 +0530

    import file

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -8,6 +8,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSort } from '@angular/material/sort';
 import { ActivatedRoute } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-reports',
@@ -15,6 +17,7 @@ import { MatPaginator } from '@angular/material/paginator';
   styleUrls: ['./reports.component.scss']
 })
 export class ReportsComponent implements OnInit {
+  arrayBuffer: any;
   selecetdFile: any;
     displayedColumns: string[];
     data: IScips[];
@@ -52,14 +55,36 @@ export class ReportsComponent implements OnInit {
   this.fileToUpload = files.item(0);
 }
 onFileUpload(event){
+  console.log(event)
   this.selecetdFile = event.target.files[0];
-  this.excelService.OnUploadFile(this.selecetdFile).subscribe()
-  // const reader = new FileReader();
-  // reader.onload = () => {
+  console.log(this.selecetdFile);
+  const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
+  // this.excelService.OnUploadFile(this.selecetdFile).subscribe()
+  const reader = new FileReader();
+  reader.onload = () => {
   // this.imagePreview = reader.result;
-  // };
-  // reader.readAsDataURL(this.selecetdFile);
-  }
+  this.arrayBuffer = reader.result;    
+  var data = new Uint8Array(this.arrayBuffer);    
+  var arr = new Array();    
+  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);    
+  var bstr = arr.join("");    
+  var workbook = XLSX.read(bstr, {type:"binary"});    
+  var first_sheet_name = workbook.SheetNames[0];    
+  var worksheet = workbook.Sheets[first_sheet_name];   
+  //json array  
+    var arraylist = XLSX.utils.sheet_to_json(worksheet,{raw:true});     
+        // this.filelist = [];    
+        console.log(arraylist)    
+        const worksheet1: XLSX.WorkSheet = XLSX.utils.json_to_sheet(arraylist);
+    const workbook2: XLSX.WorkBook = { Sheets: { 'data': worksheet1 }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook2, { bookType: 'xlsx', type: 'array' });
+    const data1: Blob = new Blob([excelBuffer], {type: EXCEL_TYPE});
+    const fileName = 'data';
+    // FileSaver.saveAs(data1, fileName + '_export_' + new  Date().getTime() + EXCEL_EXTENSION);
+  };
+  reader.readAsArrayBuffer(this.selecetdFile);
+}
  viewScipsFields(){
   this.showSSEP = !this.showSSEP;
 }
